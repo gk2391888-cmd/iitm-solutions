bda3248f63c879a13409d08a97d873c117eee342
gk2391888-cmd <gk2391888@gmail.com>
2026-09-15 20:25:12 +0530

    Add footer to the webpage

---
+++ footer.js
@@ -0,0 +1,6 @@
+document.write(`
+<footer style="text-align:center; padding:12px; background:#0a0a0a; color:#aaa; font-size:14px; font-family: sans-serif;">
+  Owned & Managed by <b style="color:white;">THE TECH HUB</b> | UDYAM-BR-19-0017611<br>
+  IITM SOLUTIONS & FF LEAGUE are products of THE TECH HUB
+</footer>
+`);
